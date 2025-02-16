3d08134320d6671500f12258f1c49af0dbeeb808
Xunxin Li <xunxin.li@stonybrook.edu>
2025-02-16 02:06:38 -0500

    wip

--- src/pages/home/components/experience/experience.section.tsx
+++ src/pages/home/components/experience/experience.section.tsx
@@ -1,108 +1,103 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import Container from '@/components/container';
-import { Experience } from './type';
-import Icon from '@icon-park/react/es/all';
-import './experience.style.scss';
-import { ClassNames } from '@/modules/classNames';
-
+import React from "react";
+import { useTranslation } from "react-i18next";
+import Container from "@/components/container";
+import { Experience } from "./type";
+import Icon from "@icon-park/react/es/all";
+import "./experience.style.scss";
+import { ClassNames } from "@/modules/classNames";
 
 function ExperienceSection() {
-	const CN = new ClassNames('experience');
-	const { t} = useTranslation(['experience']);
-	const cloudObjectives: Array<string> = new Array(9).fill(0).map((item, index) => t(`cloud.${index}`));
-	const sensifaiObjectives: Array<string> = new Array(7).fill(0).map((item, index) => t(`sensifai.${index}`));
-	const garousianObjectives: Array<string> = new Array(5).fill(0).map((item, index) => t(`garousian.${index}`));
-	const noxowlObjectives: Array<string> = new Array(5).fill(0).map((item, index) => t(`noxowl.${index}`));
-	const iVahidObjectives: Array<string> = new Array(4).fill(0).map((item, index) => t(`ivahid.${index}`));
-	const rabinObjectives: Array<string> = new Array(3).fill(0).map((item, index) => t(`rabin.${index}`));
+  const CN = new ClassNames("experience");
+  const { t } = useTranslation(["experience"]);
+  const cloudObjectives: Array<string> = new Array(9)
+    .fill(0)
+    .map((item, index) => t(`cloud.${index}`));
+  const sensifaiObjectives: Array<string> = new Array(7)
+    .fill(0)
+    .map((item, index) => t(`sensifai.${index}`));
+  const garousianObjectives: Array<string> = new Array(5)
+    .fill(0)
+    .map((item, index) => t(`garousian.${index}`));
+  const noxowlObjectives: Array<string> = new Array(5)
+    .fill(0)
+    .map((item, index) => t(`noxowl.${index}`));
+  const iVahidObjectives: Array<string> = new Array(4)
+    .fill(0)
+    .map((item, index) => t(`ivahid.${index}`));
+  const rabinObjectives: Array<string> = new Array(3)
+    .fill(0)
+    .map((item, index) => t(`rabin.${index}`));
 
-	const experiencesList: Array<Experience> = [
-		{
-			title: 'Arvan Cloud Co.',
-			type: 'Hybrid',
-			position: 'Senior JavaScript Engineer',
-			location: 'Tehran, Iran',
-			date: 'Sep. 2021 - Present',
-			translation_key: 'cloud',
-			objectives: cloudObjectives,
-		},
-		{
-			title: 'Sensifai Co.',
-			type: 'Remote',
-			position: 'Senior Front-End Developer',
-			location: 'Belgium',
-			date: 'Sep. 2018 - Sep. 2021',
-			translation_key: 'sensifai',
-			objectives: sensifaiObjectives,
-		},
-		{
-			title: 'Garousian Studio',
-			type: 'Contract',
-			position: 'Front-End & WordPress Developer',
-			location: 'Tehran, Iran',
-			date: 'Oct. 2017 - Dec. 2020',
-			translation_key: 'garousian',
-			objectives: garousianObjectives,
-		},
-		{
-			title: 'NoxOwl',
-			type: 'Remote',
-			position: 'Front-End Developer',
-			location: 'Tehran, Iran',
-			date: 'Dec. 2017 - Jul. 2018',
-			translation_key: 'noxowl',
-			objectives: noxowlObjectives,
-		},
-		{
-			title: 'iVahid',
-			type: 'On-Site',
-			position: 'Front-End & WordPress Developer',
-			location: 'Tehran, Iran',
-			date: 'Apr. 2017 - Oct. 2017',
-			translation_key: 'ivahid',
-			objectives: iVahidObjectives,
-		},
-		{
-			title: 'Rabin',
-			type: 'On-Site',
-			position: 'Front-End Developer',
-			location: 'Tehran, Iran',
-			date: 'Apr. 2016 - Nov. 2016',
-			translation_key: 'rabin',
-			objectives: rabinObjectives,
-		}
-	];
+  const experiencesList: Array<Experience> = [
+    {
+      title: "Stony Brook University",
+      type: "Full Time",
+      position: "Computer Science Bachelor Degree",
+      location: "Stony Brook, NY",
+      date: "Aug. 2018 - Present",
+      translation_key: "cloud",
+      objectives: [
+        "Taken 91 credits required by computer science major in Stony Brook University",
+      ],
+    },
+    {
+      title: "Walmart",
+      type: "Remote",
+      position: "Software Engineer",
+      location: "California",
+      date: "May. 2022 - Feb. 2023",
+      translation_key: "cloud",
+      objectives: [
+        "Maintained an existing SPA and added new UI features using HTML5, Material UI, CSS3, JavaScript, React, and React Native.",
+        "Enhanced the application’s functionality using the Redux-Saga.",
+        "Performed unit testing through react-testing-library to keep up the code coverage for the application.",
+        "Ensured that the technical solution fits into the overall product architecture and design through working with peer engineers",
+        "Reported to the Scrum master regularly and participated effectively in various Agile development activities.",
+      ],
+    },
+    {
+      title: "Lambda School",
+      type: "Full Time",
+      position: "Full-Stack Web Developer",
+      location: "Remote",
+      date: "January 2021 - August 2021 ",
+      translation_key: "cloud",
+      objectives: [
+        "Completed successfully completing the required 960 hours course of study for Full Stack Web Development Certification",
+      ],
+    },
+  ];
 
-	return (
-		<section className={CN.generate()}>
-			<span className={CN.generate('circle', ['one'])} />
-			<span className={CN.generate('circle', ['two'])} />
-			<span className={CN.generate('circle', ['three'])} />
-			<span className={CN.generate('circle', ['four'])} />
-			<Container>
-				{experiencesList.map((item: Experience) => (
-					<div className={CN.generate('item')}>
-						<span className={CN.generate('bullet')}></span>
-						<span>{item.title} <em>({item.type})</em></span>
-						<h2>{ item.position }</h2>
-						<div className={CN.generate('details')}>
-							<span>{ item.location }</span>
-							<p>{ item.date }</p>
-						</div>
-						<ul>
-							{item.objectives.map((objective: string, index: number) => (
-								<li className={CN.generate('objective')} key={`${item.translation_key}_${index}`}>
-									<Icon type={'Dot'} theme={'outline'} size={24} />
-									<p>{objective}</p>
-								</li>
-							))}
-						</ul>
-					</div>
-				))}
-			</Container>
-		</section>
-	);
+  return (
+    <section className={CN.generate()}>
+      <Container>
+        {experiencesList.map((item: Experience) => (
+          <div className={CN.generate("item")}>
+            <span className={CN.generate("bullet")}></span>
+            <span>
+              {item.title} <em>({item.type})</em>
+            </span>
+            <h2>{item.position}</h2>
+            <div className={CN.generate("details")}>
+              <span>{item.location}</span>
+              <p>{item.date}</p>
+            </div>
+            <ul>
+              {item.objectives.map((objective: string, index: number) => (
+                <li
+                  className={CN.generate("objective")}
+                  key={`${item.translation_key}_${index}`}
+                >
+                  <Icon type={"Dot"} theme={"outline"} size={24} />
+                  <p>{objective}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </Container>
+    </section>
+  );
 }
 
 export default ExperienceSection;
--- src/pages/home/components/medium/medium.section.tsx
+++ src/pages/home/components/medium/medium.section.tsx
@@ -1,46 +1,66 @@
-import React from 'react';
-import Container from '@/components/container';
-import FlexRow from '@/components/flex-row';
-import FlexCol from '@/components/flex-col';
-import useMediums from '@/hooks/useMediums';
-import { ClassNames } from '@/modules/classNames';
-import { MediumCard } from './type';
-import './medium.style.scss';
-
+import React, { useState } from "react";
+import Container from "@/components/container";
+import FlexRow from "@/components/flex-row";
+import FlexCol from "@/components/flex-col";
+import { ClassNames } from "@/modules/classNames";
+import { MediumCard } from "./type";
+import potluck from "../../../../imgs/potluck.png";
+import usemytech from "../../../../imgs/usemytech.png";
+import "./medium.style.scss";
 
 function MediumSection() {
-	const CN = new ClassNames('medium');
-	const { items } = useMediums();
-	return (
-		<section className={CN.generate('')}>
-			<Container>
-				<FlexRow stretch={true}>
-					{items.map((item: MediumCard, index: number) => (
-						<FlexCol
-							xs={24}
-							sm={12}
-							md={8}
-							xl={6}
-							key={index}
-						>
-							<a
-								className={CN.generate('card')}
-								href={item.link}
-								target={'_blank'}
-								rel="noreferrer"
-							>
-								<figure>
-									<img src={item.image} alt={item.title}/>
-								</figure>
-								<h3>{item.title}</h3>
-								<p>{item.description}</p>
-							</a>
-						</FlexCol>
-					))}
-				</FlexRow>
-			</Container>
-		</section>
-	);
+  const CN = new ClassNames("medium");
+  const [items, setItems] = useState([
+    {
+      link: "https://frontend-17dulzzug-blackenjoy27.vercel.app/",
+      image: `${potluck}`,
+      title: "Potluck Planner",
+      description: "An web app helps organize potluck events~",
+    },
+    {
+      link: "https://use-my-tech-fe-blackenjoy27.vercel.app/",
+      image: `${usemytech}`,
+      title: "Use My Tech",
+      description: "An web app helps renting devices~",
+    },
+  ]);
+  return (
+    <section className={CN.generate("")}>
+      <Container>
+        <FlexRow stretch={true}>
+          {items.map((item: MediumCard, index: number) => (
+            <FlexCol xs={24} sm={12} md={8} xl={6} key={index}>
+              <a
+                className={CN.generate("card")}
+                href={item.link}
+                target={"_blank"}
+                rel="noreferrer"
+              >
+                <figure>
+                  <img src={item.image} alt={item.title} />
+                </figure>
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+              </a>
+            </FlexCol>
+          ))}
+        </FlexRow>
+        <footer
+          style={{
+            width: "20rem",
+            marginTop: "2rem",
+            marginLeft: "auto",
+            border: "1px solid grey",
+            borderRadius: "1rem",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <span>© Kyle Li 2025</span>
+        </footer>
+      </Container>
+    </section>
+  );
 }
 
 export default MediumSection;
